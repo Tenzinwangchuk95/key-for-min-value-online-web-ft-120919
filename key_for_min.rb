9306a328e27c4def17f409ad2e34452cec9799e0
Tenzin Wangchuk <tenzinwangchuk95@gmail.com>
2019-12-13 15:54:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  value_array = []
+  value_array << name_hash.each.collect do |key, value|
+    value
+  end
+  number = value_array.max
+  value_array.each.collect do |value|
+    while value < number
+      number = value
+    end
+  end
 end
