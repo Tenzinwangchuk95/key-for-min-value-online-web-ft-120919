526d83d1f10bf47d337aec45f945d27d5f04c550
Tenzin Wangchuk <tenzinwangchuk95@gmail.com>
2019-12-13 17:26:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,13 +1,14 @@
 
-
+def key_for_min_value(hash)
 
 number = 0 
 key = nil
 
-name_hash.each do |k, v|
-  if number == 0 || v < number
-    number = v
-    key = k
+  name_hash.each do |k, v|
+    if number == 0 || v < number
+      number = v
+      key = k
+    end
   end
-end
  return key
+end
